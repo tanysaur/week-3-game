b5bf55a5110110c7fc63b017e893ea7a52a33c0b
tanysaur <tanysaur@gmail.com>
2016-11-05 02:04:11 -0500

    hangman game now functional

--- Hangman Game/assets/javascript/game.js
+++ Hangman Game/assets/javascript/game.js
@@ -10,13 +10,17 @@
 var points = 0;
 var lettersGuessedSoFar = [];
 var guessesLeft = 10;
-var letters = ["a", "b", "c", "d", "e","f","g", "h", "i", "j", "k","l","m", "n", "o", "p", "q","r","s", "t", "u", "v", "w","x","y","z"];
+var letters = ["a", "b", "c", "d", "e","f","g", "h", "i", "j", "k","l",
+				"m", "n", "o", "p", "q","r","s", "t", "u", "v", "w","x","y","z"];
 var letterArr = [];
-var words = ["rick", "daryl", "glenn", "maggie", "carl", "michonne", "carol", "morgan", "abraham", "sasha", "rosita", "eugene", "judith", "enid", "spencer", "aaron", "tara", "jesus"];
+var words = ["rick", "daryl", "glenn", "maggie",
+			"carl", "michonne", "carol", "morgan", 
+			"abraham", "sasha", "rosita", "eugene", 
+			"judith", "enid", "spencer", "aaron", 
+			"tara", "jesus"];
 var myWord = null;
 var blanks = [];
 var correctGuesses = [];
-var userGuessedCorrect = false;
 var gameOver = false;
 
 //when page loads
@@ -29,58 +33,46 @@ $(document).ready(function(){
  $(document).keyup(function() {
 	
 	//game not over, user has guesses left
-	if(!gameOver && !userGuessedCorrect && words.length > 0){
+	if(!gameOver && words.length > 0){
 		var userInput = String.fromCharCode(event.keyCode).toLowerCase();
 
 		for (i = 0; i < letterArr.length; i++){
-			//letters from selected word is put in an array equals the letter typed
+			//letters from selected word is put in an array 
 			if(letterArr[i] == userInput) {
+				//dashes then replaced with actual letter if input is correct
 				blanks[i] = userInput;
+				//display in html
 				$("#randomWord").text(blanks.join(''));
 			}
 		}
-		isGuessCorrect();
-	} 
-	//for every incorrect letter guessed, show in guessesLeft field and decrease guessesLeft counter
-	if(letters.includes(userInput) && letterArr.indexOf(userInput) == -1 && lettersGuessedSoFar.indexOf(userInput) == -1){
-		lettersGuessedSoFar.push(userInput);
-		$("#guessedArr").text(lettersGuessedSoFar);
-		guessesLeft--;
-		$("#guessesLeft").text(guessesLeft);
-		console.log(guessesLeft);
-	}
-	//checks if game is over
-	isGameOver();
 
-	//has user answered / used up all words?
-	isGameComplete();
+		//for every incorrect letter guessed, show in guessesLeft field; do not re-use letters already guessed
+		if(letters.includes(userInput) && letterArr.indexOf(userInput) == -1 && lettersGuessedSoFar.indexOf(userInput) == -1){
+			lettersGuessedSoFar.push(userInput);
+			//display letters guessed in html
+			$("#guessedArr").text(lettersGuessedSoFar);
+			//decrease guessesLeft counter
+			guessesLeft--;
+			//display guesses left in html
+			$("#guessesLeft").text(guessesLeft);
+		}
 
-	if(userGuessedCorrect) {
-		nextWord();
+		//check if user guessed correctly, if so, generate next word
+		isGuessCorrect(); 
+		//checks if game is over
+		isGameOver();
+		//has user answered / used up all words?
+		isGameComplete();
 	}
 })
 
-//resets counters for next word
-function nextWord(){
-	gameOver = false;
-	letterArr = [];
-	blanks = [];
-	selectWord();	
-	displayBlanks();
-	lettersGuessedSoFar = [];
-	$("#guessedArr").text(lettersGuessedSoFar);
-	guessesLeft = 10;
-	$("#guessesLeft").text(guessesLeft);
-}
-
 //random word selection
 function selectWord(){
-
 	if (words.length > 0){
 		myWord = words[Math.floor(Math.random()*words.length)];
 		letterArr = myWord.split(''); //delimiter
 	}
-	console.log(myWord);
+	//console.log(letterArr);
 }
 
 //displays '-' for each letter in the selected word array
@@ -88,15 +80,27 @@ function displayBlanks(){
 	for (i = 0; i < letterArr.length; i++){
 			blanks[i] = "-";
 		}
-	$("#randomWord").html(blanks);
-	console.log(blanks);
+	$("#randomWord").html(blanks); //display in html
 }
 
-
+//resets counters and displays for next word
+function nextWord(){
+	gameOver = false;
+	letterArr = [];
+	blanks = [];
+	selectWord();	
+	displayBlanks();
+	guessesLeft = 10;
+	$("#guessesLeft").text(guessesLeft); //display in html
+	lettersGuessedSoFar = [];
+	$("#guessedArr").text(lettersGuessedSoFar); //display in html
+	
+}
 
 //checks if game is over
 function isGameOver(){
 	if(guessesLeft <= 0){
+		//triggers walker audio
 		$("#walkerGrowl").trigger('play');
 		gameOver = true;
 		alert("You didn't save " + myWord.toUpperCase() + " from the walkers!");
@@ -104,7 +108,8 @@ function isGameOver(){
 		if (playAgain){
 			reset();
 		} else{
-			$("#lizzie-freakout").trigger('play');
+			//if user doesn't want to play again, this triggers lizzie's freakout audio
+			$("#lizzieFreakout").trigger('play');
 			//try modal images for pop-up walkers
 		}
 	}
@@ -112,15 +117,15 @@ function isGameOver(){
 
 //check if user guessed correct word
 function isGuessCorrect(){
-	if (blanks.indexOf("-") == -1) { //no '-' left. all letters exposed
+	//no '-' left. all letters now exposed / guessed
+	if (blanks.indexOf("-") == -1) { 
 		points++;
-		$("#escapedCount").text(points);
-		correctGuesses.push(myWord);
-		console.log(myWord);
-		//correct guess array is not empty
-    	$("#" + myWord).addClass("blurOff");  //show image of guessed word
+		$("#escapedCount").text(points); //display in html
+		correctGuesses.push(myWord); //stores guessed word to correct guesses
+		//console.log(myWord);
+    	$("#" + myWord).addClass("blurOff");  //show image of guessed word in html
     	words.splice(words.indexOf(myWord),1); // omit myWords that have been used before			
-    	console.log(words.length);		
+    	//console.log(words.length);		
 		nextWord();
 	}
 }
@@ -135,10 +140,9 @@ function isGameComplete(){
 }
 
 
-//for when user "tries again"
+//for when user plays the game again, this resets everything
 function reset(){
 	gameOver = false;
-	userGuessedCorrect = false;
 	points = 0;
 	$("#escapedCount").text(points);
 	letterArr = null;
@@ -147,15 +151,19 @@ function reset(){
 	$("#guessedArr").text(lettersGuessedSoFar);
 	guessesLeft = 10;
 	$("#guessesLeft").text(guessesLeft);
-	var resetWords = ["rick", "daryl", "glenn", "maggie", "carl", "michonne", "carol", "morgan", "abraham", "sasha", "rosita", "eugene", "judith", "enid", "spencer", "aaron", "tara", "jesus"];
+	var resetWords = ["rick", "daryl", "glenn", "maggie", 
+					"carl", "michonne", "carol", "morgan", 
+					"abraham", "sasha", "rosita", "eugene", 
+					"judith", "enid", "spencer", "aaron", 
+					"tara", "jesus"];
 	words = resetWords;
 	blurReset();
 	selectWord();
 	displayBlanks();
 }
 
+//displays non-blurry images
 function blurReset() {
-	
 	for(var i = 0; i < words.length; i++) {
 		$("#" + words[i]).removeClass("blurOff");
 	}
